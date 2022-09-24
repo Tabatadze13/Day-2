d0193c870d2ac946a75cac50ff9a2fcfac43b8fd
ilia <112582300+Tabatadze13@users.noreply.github.com>
2022-09-24 18:50:24 +0400

    Add files via upload

---
+++ Lesson2.py
@@ -0,0 +1,34 @@
+
+
+# my_sentence = """Hello, I'm ilia tabatadze. I am from Georgia and im 13
+# years old <3
+# I love programming"""
+
+
+# knows_programming = True
+
+
+# if 10>5:
+#   print("hello")
+
+
+
+# full_name ="ilia tabatadze"
+
+# print(len(full_name))
+
+# print(full_name[7])
+# #position = index (Thynonym)
+
+# print("i" not in full_name)
+
+full_name ="ilia tabatadze"
+# print(full_name[2:9])
+# print(full_name[2:9:2])
+
+# print(full_name[:5])
+# print(full_name[5:])
+
+# print(full_name[-1] + "lene")
+
+print(full_name[-1:-6:-1])
